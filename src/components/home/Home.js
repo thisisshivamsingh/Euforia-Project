91124c376a5f27bc73a5e1b341901947e3ec028b
Shivam <thisisshivam06@gmail.com>
2022-09-27 23:06:36 +0530

    Html Part Completed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
 import img from "../../assets/logo/euforia-logo.png";
 import imgA from "../../assets/header/header.jpg";
+import imgB from "../../assets/inner/inner.png";
+import imgC from "../../assets/magento_themes/magento-themeA.jpg";
+import imgD from "../../assets/magento_themes/magento-themeB.jpg";
+import imgE from "../../assets/magento_themes/magento-themeC.jpg";
+import imgF from "../../assets/layouts/web1.png";
+import imgG from "../../assets/layouts/web2.png";
+import imgH from "../../assets/layouts/web3.png";
 import "./Home.css";
 
 const Home = () => {
@@ -16,10 +23,59 @@ const Home = () => {
       <h1 className="choose-your-demo">Choose Your Demo</h1>
       <p className="homepage-layouts">3 Homepage Layouts</p>
       <div className="layouts">
-        <div className="layout"></div>
-        <div className="layout"></div>
-        <div className="layout"></div>
+        <div className="layout">
+          <img src={imgF} alt="homepage-layout" className="homepage-layout" />
+        </div>
+        <div className="layout">
+          <img src={imgG} alt="homepage-layout" className="homepage-layout" />
+        </div>
+        <div className="layout">
+          <img src={imgH} alt="homepage-layout" className="homepage-layout" />
+        </div>
       </div>
+      <div className="layouts-names">
+        <h3 className="layout-name">FASHION</h3>
+        <h3 className="layout-name">ELECTRONIC</h3>
+        <h3 className="layout-name">JEWELLERY</h3>
+      </div>
+      <button className="purchase-now-btn">
+        <span className="purchase-now-text">Purchase Now!</span>
+      </button>
+      <h1 className="creating-website-today">
+        Start Creating Your Website Today With
+      </h1>
+      <h1 className="euforia-theme-now">Euforia Theme Now</h1>
+      <div className="best-selling-magento-themes">
+        <img src={imgB} alt="inner" />
+        <h1>OUR BEST SELLING MAGENTO THEMES</h1>
+        <div className="magento-themes">
+          <div className="magento-theme">
+            <img
+              src={imgC}
+              alt="magento-themeA"
+              className="magento-theme-img"
+            />
+          </div>
+          <div className="magento-theme">
+            <img
+              src={imgD}
+              alt="magento-themeB"
+              className="magento-theme-img"
+            />
+          </div>
+          <div className="magento-theme">
+            <img
+              src={imgE}
+              alt="magento-themeC"
+              className="magento-theme-img"
+            />
+          </div>
+        </div>
+      </div>
+      <p className="rights-reserved">
+        @2016 Euforia theme. All rights reserved. Magento themes by
+        <span className="best-magneto-theme"> bestmagentotheme</span>
+      </p>
     </>
   );
 };
